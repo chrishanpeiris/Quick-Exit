926bcb56c0d2b24cfc64e360413dcbb431de588d
Chris Mackay <tf.sitemaster@nt.gov.au>
2020-05-28 07:02:26 +0930

    Coronavirus specific project files

--- assets/quickexit.js
+++ assets/quickexit.js
@@ -1,26 +1,25 @@
+function quickExit() {
+  // Quick Exit to open new Google tab
+  window.open("http://www.google.com", "_newtab");
+  // Quick Exit to replace current site with NTGov home page
+  window.location.replace('https://nt.gov.au/');
+}
 
-    function quickExit() {
-      // Quick Exit to open new BOM tab
-      window.open("http://www.google.com", "_newtab");
-      // Quick Exit to replace current site with NTGov home page
-      window.location.replace('https://nt.gov.au/');
+$(function() {
+
+  $("#quickexit").on("click", function(e) {
+    quickExit();
+  });
+
+  $("#quickexit a").on("click", function(e) {
+    // allow the link to work
+    e.stopPropagation();
+  });
+
+  $(document).keyup(function(e) {
+    if (e.keyCode == 27) { // allows escape key
+      quickExit();
     }
+  });
 
-    $(function() {
-
-      $("#quickexit").on("click", function(e) {
-        quickExit();
-      });
-
-      $("#quickexit a").on("click", function(e) {
-        // allow the link to work
-        e.stopPropagation();
-      });
-
-      $(document).keyup(function(e) {
-        if (e.keyCode == 27) { // allows escape key
-          quickExit();
-        }
-      });
-
-    });
+});
